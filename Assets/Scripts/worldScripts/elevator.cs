289dbbfa213ff088bb96401d9014a966dfc3ecfa
Jax <buzzingjax@gmail.com>
2025-10-04 20:05:05 -0400

    Tutorial Level mapping out

--- Assets/Scripts/worldScripts/elevator.cs
+++ Assets/Scripts/worldScripts/elevator.cs
@@ -3,19 +3,22 @@ using UnityEngine;
 
 public class elevator : MonoBehaviour
 {
-    private Transform startpoint;
+    private Vector3 startpoint;
     public GameObject endpoint;
     private Vector3 endpointCoordinates;
 
     private bool elevatorActive = false;
-    private bool cycle = false;
+    private bool moveUp = true;
+    private bool moveDown = false;
+    private bool finishCycle = false;
     public float elevatorSpeed = 3.0f;
-    private float counter = 0;
+    private float delay = 0;
+    public float timeStoppedAtEnds = 5.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        startpoint = transform;
+        startpoint = this.gameObject.transform.position;
         if(endpoint == null)
         {
             endpointCoordinates = new Vector3(transform.position.x, transform.position.y + 10.0f, transform.position.z);
@@ -29,27 +32,45 @@ public class elevator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        counter += Time.deltaTime;
-        if(elevatorActive && !cycle)
+        if((finishCycle || elevatorActive) && moveUp)
         {
             if(this.transform.position.y < endpointCoordinates.y)
             {
-                transform.Translate(Vector3.up * Time.deltaTime * elevatorSpeed, Space.Self);
+                transform.Translate(Vector3.up * Time.deltaTime * elevatorSpeed);
             }
-        }
-        if(elevatorActive && cycle)
-        {
-            if (this.transform.position.y > startpoint.position.y)
+            if(this.transform.position.y >= endpointCoordinates.y && !moveDown)
             {
-                transform.Translate(Vector3.down * Time.deltaTime * elevatorSpeed, Space.Self);
+                delay += Time.deltaTime;
+                if(delay > timeStoppedAtEnds)
+                {
+                    moveUp = false;
+                    moveDown = true;
+                    delay = 0;
+                }
             }
         }
-
-        if((this.transform.position.y >= endpointCoordinates.y || this.transform.position.y <= startpoint.position.y) && counter > 1)
+        if((finishCycle || elevatorActive) && moveDown)
         {
-            cycle = !cycle;
-            counter = 0;
+            if(this.transform.position.y > startpoint.y)
+            {
+                transform.Translate(Vector3.down * Time.deltaTime * elevatorSpeed);
+            }
+            if (this.transform.position.y <= startpoint.y && !moveUp)
+            {
+                delay += Time.deltaTime;
+                if (delay > timeStoppedAtEnds)
+                {
+                    moveUp = true;
+                    moveDown = false;
+                    delay = 0;
+                }
+                if (finishCycle)
+                {
+                    finishCycle = false;
+                }
+            }
         }
+       
     }
 
     public void OnTriggerStay(Collider other)
@@ -58,5 +79,21 @@ public class elevator : MonoBehaviour
         {
             elevatorActive = true;
         }
+        if(other.gameObject.GetComponent<Rigidbody>() != null && moveUp)
+        {
+            other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * elevatorSpeed, ForceMode.Force);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "player")
+        {
+            elevatorActive = false;
+        }
+        if(this.transform.position.y >= startpoint.y && this.transform.position.y <= endpointCoordinates.y)
+        {
+            finishCycle = true;
+        }    
     }
 }
